9d31c1e53e8bc4dc065b7842ecaac7328b228f35
John Jorgensen <john@jorgensenfamily.us>
2024-06-15 05:15:22 -0700

    add firefox extension configs

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -1,7 +1,17 @@
 { config, pkgs, ... }:
 
+let
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+        inherit pkgs;
+    };
+in
 {
     programs.firefox = {
         enable = true;
+        profiles.john = {
+            extensions = with nur.repos.rycee.firefox-addons; [
+                bitwarden
+            ];
+        };
     };
 }
