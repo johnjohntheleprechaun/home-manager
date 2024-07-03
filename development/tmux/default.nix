d09f1b79f99a60ea14d47a6f3cdbb2f714a286e0
John Jorgensen <john@jorgensenfamily.us>
2024-07-02 17:18:13 -0700

    added some more tmux config, mostly to get neovim to stop whining about truecolor

--- development/tmux/default.nix
+++ development/tmux/default.nix
@@ -5,5 +5,9 @@
         enable = true;
         clock24 = true;
         escapeTime = 10;
+        baseIndex = 1;
+        keyMode = "vi";
+        terminal = "screen-256color";
+        extraConfig = builtins.readFile ./extra.conf;
     };
 }
