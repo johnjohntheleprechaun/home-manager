ee69663f2c9a8ff38a06a7557f78a094682b7171
John Jorgensen <john@leprechaun.dev>
2024-09-18 16:22:35 -0600

    add tmux command

--- development/ssh.nix
+++ development/ssh.nix
@@ -1,5 +1,15 @@
 { config, pkgs, ... }:
 
+let
+    tmuxCommand = {
+        extraOptions = {
+            # automatically launch TMUX if it exists
+            RemoteCommand = "bash -l -c '(tmux attach-session || tmux new-session)'";
+            RequestTTY = "yes";
+            SetEnv = "TERM=xterm-256color";
+        };
+    };
+in
 {
     programs.ssh = {
         enable = true;
@@ -7,21 +17,21 @@
             "leopard.local" = {
                 hostname = "leopard";
                 user = "john";
-            };
+            } // tmuxCommand;
             "rpi4.local" = {
                 hostname = "johnpi";
                 user = "pi";
-            };
+            } // tmuxCommand;
             "leopard.remote" = {
                 hostname = "leopard.van-pogona.ts.net";
                 port = 22;
                 user = "john";
-            };
+            } // tmuxCommand;
             "rpi4.remote" = {
                 hostname = "johnpi.van-pogona.ts.net";
                 port = 22;
                 user = "pi";
-            };
+            } // tmuxCommand;
             "router" = {
                 hostname = "openwrt.lan";
                 user = "root";
