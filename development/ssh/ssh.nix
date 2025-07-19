023cae596eb5cf95cdf259b2c11f6cb52e0e9fa9
John Jorgensen <john@leprechaun.dev>
2025-07-18 19:59:29 -0600

    update ssh config (port map change)

--- development/ssh/ssh.nix
+++ development/ssh/ssh.nix
@@ -1,48 +1,49 @@
-{ config, pkgs, ... }:
-
-let
-    hostMap = import ./host-mapper.nix;
-in
 {
-    programs.ssh = {
-        enable = true;
-        matchBlocks = hostMap {
-            "leopard.local" = {
-                hostname = "leopard";
-                user = "john";
-            };
-            "rpi4.local" = {
-                hostname = "johnpi";
-                user = "pi";
-            };
-            "leopard.remote" = {
-                hostname = "leopard.van-pogona.ts.net";
-                port = 22;
-                user = "john";
-            };
-            "rpi4.remote" = {
-                hostname = "johnpi.van-pogona.ts.net";
-                port = 22;
-                user = "pi";
-            };
-            "noduh.mc" = {
-                hostname = "mc.noduh.dev";
-                port = 1822;
-                user = "bob";
-            };
-            "john.noduh.mc" = {
-                hostname = "mc.noduh.dev";
-                port = 1822;
-                user = "john";
-            };
-            "leprechaun.mc" = {
-                hostname = "mc.leprechaun.dev";
-                port = 1922;
-                user = "john";
-            };
-        };
-        extraOptionOverrides = {
-            EnableEscapeCommandline = "true";
-        };
+  config,
+  pkgs,
+  ...
+}: let
+  hostMap = import ./host-mapper.nix;
+in {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = hostMap {
+      "leopard.local" = {
+        hostname = "leopard";
+        user = "john";
+      };
+      "rpi4.local" = {
+        hostname = "johnpi";
+        user = "pi";
+      };
+      "leopard.remote" = {
+        hostname = "leopard.van-pogona.ts.net";
+        port = 22;
+        user = "john";
+      };
+      "rpi4.remote" = {
+        hostname = "johnpi.van-pogona.ts.net";
+        port = 22;
+        user = "pi";
+      };
+      "noduh.mc" = {
+        hostname = "mc.noduh.dev";
+        port = 1822;
+        user = "bob";
+      };
+      "john.noduh.mc" = {
+        hostname = "mc.noduh.dev";
+        port = 1822;
+        user = "john";
+      };
+      "leprechaun.mc" = {
+        hostname = "mc.leprechaun.dev";
+        port = 1722;
+        user = "john";
+      };
     };
+    extraOptionOverrides = {
+      EnableEscapeCommandline = "true";
+    };
+  };
 }
