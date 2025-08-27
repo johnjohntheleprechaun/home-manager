1be2e9adc58a2d6f14fbf104aafdc8bf49aa2123
John Jorgensen <john@leprechaun.dev>
2025-08-27 15:58:13 -0600

    format ssh host mapper

--- development/ssh/host-mapper.nix
+++ development/ssh/host-mapper.nix
@@ -1,18 +1,23 @@
-hostList:
-let
-    tmuxCommand = {
-        extraOptions = {
-            # automatically launch TMUX if it exists
-            RemoteCommand = "bash -l -c 'tmux attach-session || tmux new-session'";
-            RequestTTY = "yes";
-            SetEnv = "TERM=xterm-256color";
-        };
+hostList: let
+  tmuxCommand = {
+    extraOptions = {
+      ForwardAgent = "yes";
+      # automatically launch TMUX if it exists
+      RemoteCommand = "bash -l -c 'tmux attach-session || tmux new-session'";
+      RequestTTY = "yes";
+      SetEnv = "TERM=xterm-256color";
     };
-    pkgs = import <nixpkgs> {};
-    lib = pkgs.lib;
+  };
+  pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
 in
-    builtins.listToAttrs (
-        lib.mapAttrsToList (name: value: { name = name + ".raw"; value = value; }) hostList
-    ) // (
-        builtins.mapAttrs (name: value: value // tmuxCommand) hostList
-    )
+  builtins.listToAttrs (
+    lib.mapAttrsToList (name: value: {
+      name = name + ".raw";
+      value = value;
+    })
+    hostList
+  )
+  // (
+    builtins.mapAttrs (name: value: value // tmuxCommand) hostList
+  )
