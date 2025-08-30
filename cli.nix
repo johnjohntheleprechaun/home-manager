9394dfad38e68bc9613448516749f6e1c05e43b2
John Jorgensen <john@leprechaun.dev>
2025-08-30 01:41:54 -0600

    add lsof to cli pkgs

--- cli.nix
+++ cli.nix
@@ -1,18 +1,21 @@
-{ config, pkgs, ... }:
-
 {
-    home.packages = with pkgs; [
-        # CLI packages
-        usbutils
-        zip
-        unzip
-        file
-        ssh-import-id
-        ffmpeg
-        dig
-        openssl
-        binutils
-        jq
-        trash-cli
-    ];
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    # CLI packages
+    usbutils
+    zip
+    unzip
+    file
+    ssh-import-id
+    ffmpeg
+    dig
+    openssl
+    binutils
+    jq
+    trash-cli
+    lsof
+  ];
 }
