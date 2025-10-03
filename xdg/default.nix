550a1093071606d4226cf2c42e530306b90d9314
John Jorgensen <john@leprechaun.dev>
2025-10-02 18:25:57 -0600

    trim down XDG settings

--- xdg/default.nix
+++ xdg/default.nix
@@ -1,25 +1,10 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{...}: {
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    #"inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-    # web
     "text/html" = ["firefox.desktop"];
     "x-scheme-handler/http" = ["firefox.desktop"];
     "x-scheme-handler/https" = ["firefox.desktop"];
     "x-scheme-handler/mailto" = ["firefox.desktop"];
-    # images
-    "image/jpeg" = ["org.nomacs.ImageLounge.desktop"];
-    "image/png" = ["org.nomacs.ImageLounge.desktop"];
-    "image/bmp" = ["org.nomacs.ImageLounge.desktop"];
-    "image/gif" = ["org.nomacs.ImageLounge.desktop"];
-    "image/tiff" = ["org.nomacs.ImageLounge.desktop"];
-    "image/webp" = ["org.nomacs.ImageLounge.desktop"];
-    "image/svg+xml" = ["org.nomacs.ImageLounge.desktop"];
     "application/x-xcf" = ["gimp.desktop"];
   };
 }
